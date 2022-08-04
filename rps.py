86557a8fd42d5b84d359a31d4b35ac9eb03868c3
holgermueller <9jholger@gmail.com>
2022-08-03 21:12:48 -0500

    began building rps gamd

---
+++ rps.py
@@ -0,0 +1,12 @@
+# Rock Paper Scissors
+
+playerChoice = ""
+computerChoice = ""
+
+
+print("Welcome to Rock Paper Scissors")
+print("Select Rock, Paper, or Scissors:")
+
+playerChoice = input()
+
+print("You selected: " + playerChoice)

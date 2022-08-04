4daa10af399fb8fcbf798a837aed7f03df9e17bd
holgermueller <9jholger@gmail.com>
2022-08-04 13:52:55 -0500

    got break statement working

--- letterGuess.py
+++ letterGuess.py
@@ -4,11 +4,18 @@ import string
 userChoice = ""
 randomLetter = random.choice(string.ascii_letters)
 
-print("Guess the secret letter: ")
-print(randomLetter)
-userChoice = input()
+while True:
+    print("Guess the secret letter: ")
+    print("Enter QUIT to exit the game.")
+    print(randomLetter)
+    userChoice = input()
 
-if userChoice == randomLetter:
-    print("You won!")
-else:
-    print("Sorry, that's not the correct letter.")
+    if userChoice == "QUIT":
+        break
+
+    if userChoice == randomLetter:
+        print("You won!")
+    else:
+        print("Sorry, that's not the correct letter.")
+
+print("Thank you for playing. See you later.")

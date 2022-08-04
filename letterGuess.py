4fcf93f2f46cca5bd7f6c963e9b66f1492c076d9
holgermueller <9jholger@gmail.com>
2022-08-04 17:10:13 -0500

    can play game

--- letterGuess.py
+++ letterGuess.py
@@ -1,13 +1,18 @@
 import random
 import string
 
+
 userChoice = ""
 randomLetter = random.choice(string.ascii_letters)
+userTries = 26
 
 while True:
     print("Guess the secret letter: ")
+    print("Hint: The Letter could be capital or lower case!")
+    print("Attempts remaining: ")
+    print(userTries)
     print("Enter QUIT to exit the game.")
-    print(randomLetter)
+
     userChoice = input()
 
     if userChoice == "QUIT":
@@ -15,7 +20,14 @@ while True:
 
     if userChoice == randomLetter:
         print("You won!")
+        userTries = 26
     else:
         print("Sorry, that's not the correct letter.")
+        userTries -= 1
+
+    if(userTries == 0):
+        print("Sorry, but ran out of lives before guessing the correct letter.")
+        print("The correct letter was " + randomLetter)
+        break
 
 print("Thank you for playing. See you later.")

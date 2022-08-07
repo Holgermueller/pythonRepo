7ebaeeb0d31750d384458e24a5d0a4ba7c2c98e8
holgermueller <9jholger@gmail.com>
2022-08-06 22:13:22 -0500

    made madlib game

---
+++ madlib.py
@@ -0,0 +1,22 @@
+noun = ''
+verb = ''
+adverb = ''
+adjective = ''
+
+
+print("Give me a noun: ")
+noun = input()
+
+print("Give me a verb: ")
+verb = input()
+
+print("Give me an adverb: ")
+adverb = input()
+
+print("Give me an adjective: ")
+adjective = input()
+
+print("There once was a " + noun + " named Cecil.")
+print("They loved to " + verb + " all day long.")
+print("They were " + adjective + " of spiders.")
+print("And they laughed in a " + adverb + " supercilious manner.")
